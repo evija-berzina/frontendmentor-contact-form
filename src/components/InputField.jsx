11e85498d34e16995515912bc70f16c8c29ba18d
eberzina <berzina3633@gmail.com>
2026-03-10 18:27:41 +0200

    Create InputField logic with state and error messages

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,19 +1,20 @@
-
-
-export function InputField() {
-
+export function InputField({person, errorMsg, handleInputChange}) {
+  
   return (
     <>
       <div>
         <label htmlFor="first-name">First Name *</label>
-        <input type="text" id="first-name" name="first-name" />
+        <input value={person.firstName} onChange={(e) => handleInputChange(e, 'firstName')} type="text" id="first-name" name="first-name" autoComplete="given-name" />
+        <p>{errorMsg.firstNameMsg}</p>
 
         <label htmlFor="last-name">Last Name *</label>
-        <input type="text" id="last-name" name="last-name" />
+        <input value={person.lastName} onChange={(e) => handleInputChange(e, 'lastName')} type="text" id="last-name" name="last-name" autoComplete="family-name" />
+        <p>{errorMsg.lastNameMsg}</p>
       </div>
 
       <label htmlFor="email">Email Adress *</label>
-      <input type="email" id="email" name="email" />
+      <input value={person.email} onChange={(e) => handleInputChange(e, 'email')} type="email" id="email" name="email" autoComplete="email" />
+      <p>{errorMsg.emailMsg}</p>
     </>
   )
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,14 +1,72 @@
+import {useState} from 'react';
 import { InputField } from "../components/InputField";
 import { QueryType } from "../components/QueryType";
 import { MessageField } from "../components/MessageField";
 import { ConsentCheckbox } from "../components/ConsentCheckbox";
 
 export function ContactPage() {
+  const nameRegex = /^[A-Za-zÀ-ž]+(?:[-' ][A-Za-zÀ-ž]+)*$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const [person, setPerson] = useState ({
+    firstName: '',
+    lastName: '',
+    email: ''
+  });
+  const [errorMsg, setErrorMsg] = useState({
+    firstNameMsg: '',
+    lastNameMsg: '',
+    emailMsg: ''
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newErrors = {
+      firstNameMsg: '',
+      lastNameMsg: '',
+      emailMsg: ''
+    };
+
+    if(!nameRegex.test(person.firstName)) {
+      newErrors.firstNameMsg = 'Please enter a valid first name';
+    }
+
+    if(!nameRegex.test(person.lastName)) {
+      newErrors.lastNameMsg = 'Please enter a valid last name';
+    }
+
+    if(!emailRegex.test(person.email)) {
+      newErrors.emailMsg = 'Please enter a valid email address';
+    }
+
+    setErrorMsg(newErrors);
+  } 
+
+  function handleInputChange (e, fieldName) {
+    setPerson({...person, [fieldName]: e.target.value})
+  }
+
+  // function handleFirstNameInput(e) {
+  //    setPerson({...person, firstName: e.target.value});
+  // }  
+
+  // function handleLastNameInput(e) {
+  //   setPerson({...person, lastName: e.target.value});
+  // }
+
+  // function handleEmailInput(e) {
+  //   setPerson({...person, email: e.target.value});
+  // }
+
   return (
     <main>
       <h1>Contact Us</h1>
-      <form action="">
-        <InputField />
+      <form action="" onSubmit={handleSubmit}>
+        <InputField
+          handleInputChange={handleInputChange}
+          person={person}
+          errorMsg={errorMsg}
+        />
         <QueryType />
         <MessageField />
         <ConsentCheckbox />
