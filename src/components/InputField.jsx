f185cf94d01ac625c30a3e309ea8731227a59015
eberzina <berzina3633@gmail.com>
2026-03-11 18:02:55 +0200

    Add attribute required

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -4,16 +4,16 @@ export function InputField({person, errorMsg, handleInputChange}) {
     <>
       <div>
         <label htmlFor="first-name">First Name *</label>
-        <input value={person.firstName} onChange={(e) => handleInputChange(e, 'firstName')} type="text" id="first-name" name="first-name" autoComplete="given-name" />
+        <input value={person.firstName} onChange={(e) => handleInputChange(e, 'firstName')} type="text" id="first-name" name="first-name" autoComplete="given-name" required />
         <p>{errorMsg.firstNameMsg}</p>
 
         <label htmlFor="last-name">Last Name *</label>
-        <input value={person.lastName} onChange={(e) => handleInputChange(e, 'lastName')} type="text" id="last-name" name="last-name" autoComplete="family-name" />
+        <input value={person.lastName} onChange={(e) => handleInputChange(e, 'lastName')} type="text" id="last-name" name="last-name" autoComplete="family-name" required />
         <p>{errorMsg.lastNameMsg}</p>
       </div>
 
       <label htmlFor="email">Email Adress *</label>
-      <input value={person.email} onChange={(e) => handleInputChange(e, 'email')} type="email" id="email" name="email" autoComplete="email" />
+      <input value={person.email} onChange={(e) => handleInputChange(e, 'email')} type="email" id="email" name="email" autoComplete="email" required />
       <p>{errorMsg.emailMsg}</p>
     </>
   )
--- src/components/QueryType.jsx
+++ src/components/QueryType.jsx
@@ -5,11 +5,11 @@ export function QueryType({errorMsg, handleRadioInput}) {
       <div>
         Query Type *
         <label>
-          <input onChange={ (e) => handleRadioInput(e)} type="radio" name="myRadio" value="general-enquiry" />
+          <input onChange={ (e) => handleRadioInput(e)} type="radio" name="myRadio" value="general-enquiry" required />
           General Enquiry
         </label>
         <label>
-          <input onChange={(e) => handleRadioInput(e)} type="radio" name="myRadio" value="support-request" />
+          <input onChange={(e) => handleRadioInput(e)} type="radio" name="myRadio" value="support-request" required />
           Support Request
         </label>
       </div>
