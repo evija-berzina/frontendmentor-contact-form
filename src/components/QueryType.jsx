bb59e4735d31338cb553b57553913151c2d111f2
eberzina <berzina3633@gmail.com>
2026-03-13 15:48:14 +0200

    Add responsive layout and error styles

--- src/components/QueryType.jsx
+++ src/components/QueryType.jsx
@@ -1,19 +1,33 @@
 export function QueryType({person, errorMsg, handleRadioInput}) {
 
   return (
-    <>
-      <div>
-        Query Type *
-        <label>
-          <input onChange={() => handleRadioInput('general-enquiry')} type="radio" name="myRadio" checked={person.queryType === "general-enquiry"} required />
-          General Enquiry
-        </label>
-        <label>
-          <input onChange={() => handleRadioInput('support-request')} type="radio" name="myRadio" checked={person.queryType === "support-request"} required />
-          Support Request
-        </label>
-      </div>
-      <p>{errorMsg.queryMsg}</p>
-    </>
+    <div className="flex flex-col">
+      <label className="pb-2">Query Type *</label>
+        <div className="flex flex-col gap-4 md:flex-row">
+          <label className="form-radio-container md:flex-1">
+            <input
+              className="w-4 h-4"
+              onChange={() => handleRadioInput('general-enquiry')}
+              type="radio"
+              name="myRadio"
+              checked={person.queryType === "general-enquiry"}
+              required
+            />
+            General Enquiry
+          </label>
+          <label className="form-radio-container md:flex-1">
+            <input
+              className="w-4 h-4"
+              onChange={() => handleRadioInput('support-request')}
+              type="radio"
+              name="myRadio"
+              checked={person.queryType === "support-request"}
+              required
+            />
+            Support Request
+          </label>
+        </div>
+      <p className="error-message">{errorMsg.queryMsg}</p>
+    </div>
   )
 }
