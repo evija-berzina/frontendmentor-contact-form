56eef25338da9bf925442019b2761a6365cb6de1
eberzina <berzina3633@gmail.com>
2026-03-13 15:01:34 +0200

    Add responsive layout and error styles

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,20 +1,56 @@
 export function InputField({person, errorMsg, handleInputChange}) {
   
   return (
-    <>
-      <div>
-        <label htmlFor="first-name">First Name *</label>
-        <input value={person.firstName} onChange={(e) => handleInputChange(e, 'firstName')} type="text" id="first-name" name="first-name" autoComplete="given-name" required />
-        <p>{errorMsg.firstNameMsg}</p>
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-4 md:flex-row">
+        <div className="flex flex-col md:flex-1">
+          <label htmlFor="first-name" className="
+          pb-2">First Name *</label>
+          <input
+            className={`${errorMsg.firstNameMsg ? 'input-error' : 'input-normal'}`}
+            value={person.firstName}
+            onChange={(e) => handleInputChange(e, 'firstName')}
+            type="text"
+            id="first-name"
+            name="first-name"
+            autoComplete="given-name"
+            required
+          />
+          <p className="error-message h-5">{errorMsg.firstNameMsg}</p>
+        </div>
 
-        <label htmlFor="last-name">Last Name *</label>
-        <input value={person.lastName} onChange={(e) => handleInputChange(e, 'lastName')} type="text" id="last-name" name="last-name" autoComplete="family-name" required />
-        <p>{errorMsg.lastNameMsg}</p>
+        <div className="flex flex-col md:flex-1">
+          <label htmlFor="last-name" className="
+          pb-2">Last Name *</label>
+          <input
+            className={`${errorMsg.lastNameMsg ? 'input-error' : 'input-normal'}`}
+            value={person.lastName}
+            onChange={(e) => handleInputChange(e, 'lastName')}
+            type="text"
+            id="last-name"
+            name="last-name"
+            autoComplete="family-name"
+            required
+          />
+          <p className="error-message h-5">{errorMsg.lastNameMsg}</p>
+        </div>
       </div>
 
-      <label htmlFor="email">Email Adress *</label>
-      <input value={person.email} onChange={(e) => handleInputChange(e, 'email')} type="email" id="email" name="email" autoComplete="email" required />
-      <p>{errorMsg.emailMsg}</p>
-    </>
+      <div className="flex flex-col">
+        <label htmlFor="email" className="
+          pb-2">Email Adress *</label>
+        <input
+          className={`${errorMsg.emailMsg ? 'input-error' : 'input-normal'}`}
+          value={person.email}
+          onChange={(e) => handleInputChange(e, 'email')}
+          type="email"
+          id="email"
+          name="email"
+          autoComplete="email"
+          required
+        />
+        <p className="error-message h-5">{errorMsg.emailMsg}</p>
+      </div>
+    </div>
   )
 }
