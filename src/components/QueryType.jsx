872023f693f2504049feb0b63235dba8df1e722e
eberzina <berzina3633@gmail.com>
2026-03-14 18:09:39 +0200

    Add background color for radio buttons

--- src/components/QueryType.jsx
+++ src/components/QueryType.jsx
@@ -4,9 +4,9 @@ export function QueryType({person, errorMsg, handleRadioInput}) {
     <div className="flex flex-col">
       <label className="pb-2">Query Type *</label>
         <div className="flex flex-col gap-4 md:flex-row">
-          <label className="form-radio-container md:flex-1">
+          <label className={`${person.queryType === "general-enquiry" ? 'checked-radio-container' : 'form-radio-container'} md:flex-1`} >
             <input
-              className="w-4 h-4"
+              className={`w-4 h-4 ${person.queryType ? 'accent-[hsl(var(--green-600))]' : ''}`}
               onChange={() => handleRadioInput('general-enquiry')}
               type="radio"
               name="myRadio"
@@ -15,9 +15,9 @@ export function QueryType({person, errorMsg, handleRadioInput}) {
             />
             General Enquiry
           </label>
-          <label className="form-radio-container md:flex-1">
+          <label className={`${person.queryType === "support-request" ? 'checked-radio-container' : 'form-radio-container'} md:flex-1`} >
             <input
-              className="w-4 h-4"
+              className={`w-4 h-4 ${person.queryType ? 'accent-[hsl(var(--green-600))]' : ''}`}
               onChange={() => handleRadioInput('support-request')}
               type="radio"
               name="myRadio"
