44ecc5ca1216b0ecdc5d852cb392b6d1977a7428
eberzina <berzina3633@gmail.com>
2026-03-15 14:55:52 +0200

    Disable hover effect

--- src/components/QueryType.jsx
+++ src/components/QueryType.jsx
@@ -6,7 +6,7 @@ export function QueryType({person, errorMsg, handleRadioInput}) {
         <div className="flex flex-col gap-4 md:flex-row">
           <label className={`${person.queryType === "general-enquiry" ? 'checked-radio-container' : 'form-radio-container'} md:flex-1`} >
             <input
-              className={`w-4 h-4 ${person.queryType ? 'accent-[hsl(var(--green-600))]' : ''}`}
+              className={`w-4 h-4 cursor-pointer ${person.queryType ? 'accent-[hsl(var(--green-600))]' : ''}`}
               onChange={() => handleRadioInput('general-enquiry')}
               type="radio"
               name="myRadio"
@@ -17,7 +17,7 @@ export function QueryType({person, errorMsg, handleRadioInput}) {
           </label>
           <label className={`${person.queryType === "support-request" ? 'checked-radio-container' : 'form-radio-container'} md:flex-1`} >
             <input
-              className={`w-4 h-4 ${person.queryType ? 'accent-[hsl(var(--green-600))]' : ''}`}
+              className={`w-4 h-4 cursor-pointer ${person.queryType ? 'accent-[hsl(var(--green-600))]' : ''}`}
               onChange={() => handleRadioInput('support-request')}
               type="radio"
               name="myRadio"
